cf260e8a1d971a89e1d7c5eab23b4b1bc7d97b12
kgs <simonudemy09@gmail.com>
2024-09-17 18:10:26 +0200

    port changed

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -36,7 +36,7 @@ export class Server {
       this.app.use('/images', imageRoutes);
 
       // Start the server
-      const PORT = process.env.PORT || 5001;
+      const PORT = process.env.PORT || 5000;
 
       this.app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
